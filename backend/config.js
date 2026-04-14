ba14c8250b98a751917c65fdf0f03fb82bc78520
Tato <freddy.rich2008@gmail.com>
2026-04-14 08:24:41 -0600

    Pruebas de emial y cookie

--- backend/config.js
+++ backend/config.js
@@ -5,4 +5,8 @@ export const config = {
     JWT:{
         secret: process.env.JWT_Secret_Key,
     },
+    email:{
+        user_email: process.env.USER_EMAIL,
+        user_password: process.env.USER_PASSWORD
+    }
 };
--- backend/src/controllers/registerCustomerController.js
+++ backend/src/controllers/registerCustomerController.js
@@ -4,7 +4,7 @@ import crypto from 'crypto'
 import jsonwebtoken from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
-
+import config from '../config.js'
 registerCustomerController = {};
 
 registerCustomerController.insertCustomer = async (req,res) => {
@@ -59,24 +59,84 @@ registerCustomerController.insertCustomer = async (req,res) => {
         },
         // Secret key
         config.JWT.secret,
+        //Expiración
         {expiresIn: '15m'}
     );
 
     //guardamos el token en una cookie
     res.cookie("resgistrationCookie",token,{maxAge: 15*60*1000})
-    
-    const newCustomer = new customerModel({
+
+    //Enviar correo electrónico
+    //1 Transporter -> ¿Qui en lo envía?
+    const transporter = nodemailer.createTransporter({
+        service: "gmail",
+        auth: {
+            user:config.email.user_email,
+            pass:config.email.user_password
+        }
+    });
+
+    // mailOptions -> ¿Quién lo recibe y cómo?
+    const mailOptiones = {
+        from: config.email.user_email,
+        to: email,
+        subject: "Verificación de cuenta",
+        text: "Código de verificación: " + randomCode + "expira en 15 minutos"
+    };
+
+    // Enviar el correo electrónico
+    transporter.sendMail(mailOptiones, (error, info)=> {
+        if(error){
+            console.log(error);
+            return res.status(500).json({message: "Error sending email"});
+        }
+
+        return res.status(200).json({message: "Email Sent"})
+    });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+//Verificar el código que se acaba de enviar
+
+registerCustomerController.verifyCode = async(req,res) => {
+    try {
+        const {verificationCodeRequest} = req.body;
+        //Extraer todo los datos del token
+        const token = req.cookies.registrationCookie;
+        const decode = jsonwebtoken.verify(token, config.JWT.secret);
+        const {
+            randomeCode: storedCode,
+            name,
+            lastName,
+            email,
+            password,
+            birthDate,
+            isVerified,
+            loginAttemps,
+            timeOut
+        } = decode;
+
+        if(verificationCodeRequest !== storedCode){
+            return res.status(400).json({message: "Invalid code"});
+        }
+        
+        // Si todo está bien, y el usuario, lo registramos en la DB
+
+     const newCustomer = new customerModel({
       name, 
       lastName, 
       email, 
       password, 
       birthDate, 
-      isVerified,
-      loginAttemps,
-      timeOut
+      isVerified: true
     });
     await newCustomer.save();
-    return res.status(201).json({message: "Customer created successfully"});
+    res.clearCookie("registrationCookie");
+    return res.status(200).json({message: "Customer created successfully"});
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: "Internal Server Error"});
